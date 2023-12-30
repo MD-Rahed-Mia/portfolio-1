95a1b25e5cf18b941989680665b13bc77f7b47a7
mdrahed2023 <135752931+mdrahed2023@users.noreply.github.com>
2023-12-30 16:17:22 +0600

    added why me section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './component/Navbar/Navbar'
 import Hero from './component/Hero/Hero'
 import AboutMe from './component/AboutMe/AboutMe'
 import Services from './component/Services/Services'
+import Whyme from './component/Whyme/Whyme'
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
         <Services />
       </div>
 
-      
+      <Whyme />
 
     </>
   )
---
+++ src/component/Whyme/Whyme.jsx
@@ -0,0 +1,49 @@
+import "./Whyme.css";
+import WhyMe from "./../../../public/images/whyme.jpeg";
+export default function Whyme() {
+  return (
+    <>
+      <div className="container">
+        <div className="why-me">
+          <div className="wm-left">
+            <img src={WhyMe} alt="whyme" />
+          </div>
+          <div className="wm-right">
+            <h1 className="wm-title">Why you choose me?</h1>
+            <div className="wm-acordion">
+              <ul>
+                <li>
+                  <h1>Up to date technologies</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus dignissimos cum obcaecati maiores pariatur vero
+                    possimus odio, enim ex soluta dicta, tempore velit ullam
+                    dolore.
+                  </p>
+                </li>
+                <li>
+                  <h1>Always meet deadline</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus dignissimos cum obcaecati maiores pariatur vero
+                    possimus odio, enim ex soluta dicta, tempore velit ullam
+                    dolore.
+                  </p>
+                </li>
+                <li>
+                  <h1>Afordable price</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus dignissimos cum obcaecati maiores pariatur vero
+                    possimus odio, enim ex soluta dicta, tempore velit ullam
+                    dolore.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
